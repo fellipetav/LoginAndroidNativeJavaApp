4b91622b16cc7ecd5d9e4b08ed95847e9c90146f
fellipetav <43867160+fellipetav@users.noreply.github.com>
2022-06-19 15:51:56 -0300

    adding String components

--- app/src/main/java/com/example/loginandroidnativejavaapp/MainActivity.java
+++ app/src/main/java/com/example/loginandroidnativejavaapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.loginandroidnativejavaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText username = (EditText) findViewById(R.id.editText1);
+        EditText password = (EditText) findViewById(R.id.editText2);
+        public void login(View view){
+            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                //correct password
+            } else{
+                //wrong password
+            }
+        }
+
+
     }
 }
