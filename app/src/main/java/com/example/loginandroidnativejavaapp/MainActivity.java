bff2c12b055d1ea813b077451a8183a6028443fe
fellipetav <43867160+fellipetav@users.noreply.github.com>
2022-06-19 18:07:30 -0300

    click action to Register Activity

--- app/src/main/java/com/example/loginandroidnativejavaapp/MainActivity.java
+++ app/src/main/java/com/example/loginandroidnativejavaapp/MainActivity.java
@@ -2,19 +2,40 @@ package com.example.loginandroidnativejavaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText formEmail;
+    EditText formPassword;
+    TextView register;
+    Button loginBtn;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText username = (EditText) findViewById(R.id.editText1);
-        EditText password = (EditText) findViewById(R.id.editText2);
+        db = new DatabaseHelper(this);
+        formEmail = (EditText) findViewById(R.id.txt_email);
+        formPassword = (EditText) findViewById(R.id.txt_password);
+        register = (TextView) findViewById(R.id.view_register);
+        loginBtn = (Button) findViewById(R.id.btn_login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
         public void login(View view){
             if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                 //correct password
